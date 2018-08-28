8d5bbca635bd08f0915ea84484d9d47dd3d89df0
mathmurdock <matheus.rodrisantos@bol.com.br>
2018-08-27 22:15:20 -0300

    Add files via upload
    Comecei a fazer o exercicio 1  da lista do professor

---
+++ exercicios_entregar/exercicios1/Banco.java
@@ -0,0 +1,12 @@
+public class Banco
+{
+   public static void main(String args[])
+   {
+       Cliente c1[]=new Cliente[4];
+       for(int i=0;i<=4;i++)
+       {
+           c1[i]=new Cliente();
+       }
+       System.out.println("Fila de banco !!");
+   }
+}
---
+++ exercicios_entregar/exercicios1/Cliente.java
@@ -0,0 +1,13 @@
+
+public class Cliente
+{
+    private String nomes; 
+    
+    String getNomes(){    
+        return nomes; 
+    }
+    void setNome(String n){
+        nomes=n;
+    }
+    
+}
